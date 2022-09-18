d409bde241ad18888e3b2933c5679e2688840e58
Edbert Voo <developer@edbertvoo.com>
2022-09-18 16:10:24 -0600

    Initial solution

--- Pandell/Program.cs
+++ Pandell/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Pandell
 {
@@ -6,7 +8,19 @@ namespace Pandell
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            IEnumerable<int> numList = Enumerable.Range(1, 10000);
+
+            Random rnd = new Random();
+
+            List<int> randList = numList.Select(x => new {val = x, order = rnd.Next()}).OrderBy(x => x.order).Select(x=> x.val).ToList();
+
+            foreach (var i in randList)
+            {
+                Console.WriteLine(i);
+            }
+
+
         }
     }
 }
